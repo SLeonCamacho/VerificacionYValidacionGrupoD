75338a406b662c1e4169d42b3c576ef709952e3a
Santiago León <74388091+SLeonCamacho@users.noreply.github.com>
2023-12-11 16:16:58 -0500

    Update script.js

--- Taller: Falla, defecto y error/script.js
+++ Taller: Falla, defecto y error/script.js
@@ -1,21 +1,34 @@
 // JavaScript: Lógica para limitar intentos de inicio de sesión con fallos y errores
 
 let intentos = 0;
+const credencialesCorrectas = {
+    username: "usuario123",
+    password: "contraseñaSegura"
+};
 
 document.getElementById('loginForm').addEventListener('submit', function(event){
     event.preventDefault();
 
-    intentos++;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-    // Fallo: Se supone que el límite es de tres intentos, pero debido a un error lógico,
-    // el sistema permite un intento adicional.
-    // Defecto: Uso incorrecto del operador, debería ser '>'
-    if (intentos >= 3) {
-        alert('Has superado el número de intentos');
-        // Error: El formulario no se bloquea después de superar los intentos
-        // Debería haber una lógica aquí para deshabilitar el formulario
+    intentos++;
+    // Verificar si las credenciales ingresadas coinciden con las credenciales correctas
+    if(username === credencialesCorrectas.username && password === credencialesCorrectas.password) {
+        alert('Inicio de sesión exitoso');
+        // Aquí se debería redirigir al usuario o cambiar la interfaz para indicar el éxito del inicio de sesión
     } else {
-        // Lógica para verificar las credenciales del usuario (simulada aquí)
-        console.log('Verificando credenciales...');
+        alert('Usuario o contraseña incorrectas');
+        // Fallo: Se supone que el límite es de tres intentos, pero debido a un error lógico,
+        // el sistema permite un intento adicional.
+        // Defecto: Uso incorrecto del operador, debería ser '>'
+        if (intentos >= 3) {
+            alert('Has superado el número de intentos');
+            // Error: El formulario no se bloquea después de superar los intentos
+            // Debería haber una lógica aquí para deshabilitar el formulario
+        } else {
+            // Lógica para verificar las credenciales del usuario (simulada aquí)
+            console.log('Verificando credenciales...');
+        }
     }
 });
