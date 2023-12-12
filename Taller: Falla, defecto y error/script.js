568e8571d58798a51d9a13dae4310ae8c6efa2e4
Santiago León <74388091+SLeonCamacho@users.noreply.github.com>
2023-12-12 08:33:50 -0500

    Update script.js

--- Taller: Falla, defecto y error/script.js
+++ Taller: Falla, defecto y error/script.js
@@ -14,8 +14,8 @@ document.getElementById('loginForm').addEventListener('submit', function(event){
 
     // Fallo: Se supone que el límite es de tres intentos, pero debido a un error lógico,
     // el sistema permite un intento adicional.
-    // Defecto: Uso incorrecto del operador, debería ser '>'
-    if (intentos >= 3) {
+    // Defecto: Uso incorrecto del operador, debería ser '>='
+    if (intentos > 3) {
         alert('Has superado el número de intentos');
         // Error: El formulario no se bloquea después de superar los intentos
     } else {
