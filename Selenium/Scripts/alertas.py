1f2e72ea6a9393ded7699b0ebcafc216cd3468e7
SLeonCamacho <74388091+SLeonCamacho@users.noreply.github.com>
2023-12-31 23:33:20 -0500

    Update alertas.py

--- Selenium/Scripts/alertas.py
+++ Selenium/Scripts/alertas.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
 
 driver = webdriver.Firefox()
-driver.get("file:///ruta/a/tu/pagina_prueba.html")  # Asegúrate de usar la ruta correcta a tu archivo HTML
+driver.get("https://sleoncamacho.github.io/VerificacionYValidacionGrupoD/Selenium/index.html")  # Asegúrate de usar la ruta correcta a tu archivo HTML
 
 # Localizar el botón por su ID
 boton = driver.find_element(By.ID, "botonAlerta")
