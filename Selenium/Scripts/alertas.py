272f8f4f6b7bb824fbc584f3ad7b2c5640ed9f66
SLeonCamacho <74388091+SLeonCamacho@users.noreply.github.com>
2023-12-31 23:29:58 -0500

    Update alertas.py

--- Selenium/Scripts/alertas.py
+++ Selenium/Scripts/alertas.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
 
 driver = webdriver.Firefox()
-driver.get("http://ejemplo.com")
+driver.get("https://sleoncamacho.github.io/VerificacionYValidacionGrupoD/Selenium/index.html")
 
 # Suponiendo que hay un botón que al hacer clic muestra una alerta
 boton = driver.find_element_by_id("botonAlerta")
